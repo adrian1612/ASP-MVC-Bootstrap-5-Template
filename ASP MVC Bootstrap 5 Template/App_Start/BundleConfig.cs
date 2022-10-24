e28856ba51ff720030ac47ea1c0ee43ed2b08ae4
AJ <AJ@IT>
2022-10-24 10:17:07 +0800

    Add project files.

---
+++ ASP MVC Bootstrap 5 Template/App_Start/BundleConfig.cs
@@ -0,0 +1,42 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace ASP_MVC_Bootstrap_5_Template
+{
+    public class BundleConfig
+    {
+        // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                        "~/Scripts/jquery-{version}.js",
+                        "~/Scripts/jquery.unobtrusive-ajax.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
+                        "~/Scripts/jquery.validate*"));
+
+            // Use the development version of Modernizr to develop with and learn from. Then, when you're
+            // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
+            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+                        "~/Scripts/modernizr-*"));
+
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+                      "~/Scripts/bootstrap.bundle.js",
+                      "~/Scripts/select2.js",
+                      "~/Scripts/DataTables/jquery.dataTables.js",
+                      "~/Scripts/DataTables/dataTables.bootstrap5.min.js",
+                      "~/Scripts/theme.js",
+                      "~/Scripts/scripts.js",
+                      "~/Scripts/respond.js"));
+
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                      "~/Content/bootstrap.css",
+                      "~/Content/css/select2.css",
+                      "~/Content/css/select2-bootstrap-5-theme.min.css",
+                      "~/Content/DataTables/css/dataTables.bootstrap5.min.css",
+                      "~/Content/fonts/fontawesome-all.min.css",
+                      "~/Content/variables.css",
+                      "~/Content/site.css"));
+        }
+    }
+}
