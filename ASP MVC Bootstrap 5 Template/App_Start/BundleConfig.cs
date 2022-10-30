b8e49eede5f518b0e00e1e226293af1395542e45
adria <adria@DESKTOP-DNK5REP>
2022-10-30 19:41:26 +0800

    Added reportviewer blueprint

--- ASP MVC Bootstrap 5 Template/App_Start/BundleConfig.cs
+++ ASP MVC Bootstrap 5 Template/App_Start/BundleConfig.cs
@@ -11,9 +11,7 @@ namespace ASP_MVC_Bootstrap_5_Template
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                         "~/Scripts/jquery-{version}.js",
                         "~/Scripts/jquery-ui-{version}.js",
-                        "~/Scripts/jquery.unobtrusive-ajax.js",
-                        "~/Scripts/jquery.validate.js",
-                        "~/Scripts/jquery.validate.unobtrusive.js"));
+                        "~/Scripts/jquery.unobtrusive-ajax.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
--- ASP MVC Bootstrap 5 Template/Controllers/HomeController.cs
+++ ASP MVC Bootstrap 5 Template/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Reporting.WebForms;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -13,18 +14,20 @@ namespace ASP_MVC_Bootstrap_5_Template.Controllers
             return View();
         }
 
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
+        //public ActionResult Print(int ID)
+        //{
+        //    ReportViewer rv = new ReportViewer(); //EXCELOPENXML FOR EXCEL & application/vnd.ms-excel FOR contentType
+        //    rv.LocalReport.ReportPath = Server.MapPath("~/Reports/Report.rdlc");
+        //    var data = new object { };
+        //    rv.LocalReport.DataSources.Add(new ReportDataSource("Report", data));
+        //    var file = rv.LocalReport.Render("pdf");
+        //    Response.ContentType = "application/pdf";
+        //    Response.AddHeader("content-disposition", $"inline;filename=Form.pdf");
+        //    Response.Buffer = true;
+        //    Response.Clear();
+        //    Response.BinaryWrite(file);
+        //    Response.End();
+        //    return View(file);
+        //}
     }
 }
