92d12bd94859b39009d32f3956934fe4325cbca3
ADMIN PC <ADMIN PC@DESKTOP-1QINIB3>
2023-06-24 19:56:23 +0800

    Update

--- ASP MVC Bootstrap 5 Template v2/Classes/Authorized.cs
+++ ASP MVC Bootstrap 5 Template v2/Classes/Authorized.cs
@@ -34,30 +34,38 @@ namespace ASP_MVC_Bootstrap_5_Template_v2
         }
     }
 
-    public class Filter : AuthorizeAttribute
+    /// <summary>
+    /// Author: Adrian Aranilla Jaspio
+    /// Position: Programmer
+    /// Date: June 23, 2023 3:30 PM
+    /// 
+    /// Subordinate: Jose Lamud Montenegro
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, Inherited = true, AllowMultiple = false)]
+    public class FilterDisplay : FilterAttribute, IAuthorizationFilter
     {
         //Change model according to structure, make sure the model has Role or filtering properties
-        object /*tbl_user*/ session { get { return HttpContext.Current.Session["user"] as object /*tbl_user*/; } }
+        /*tbl_user*/ object session { get { return HttpContext.Current.Session["user"] as /*tbl_user*/ object; } }
 
         //1) Add properties to filter
         public string Add { get; set; } = "";
         public string Edit { get; set; } = "";
         public string Delete { get; set; } = "";
 
-
-        protected override bool AuthorizeCore(HttpContextBase httpContext)
+        void FilterCore(FilterDisplay obj)
         {
+            if (obj == null) return;
             string output = string.Empty;
             var dict = new Dictionary<string[], string>();
             //2) Add items here to filter the front-end
-            dict.Add(Add.Split(','), "[data-role='Add']");
-            dict.Add(Edit.Split(','), "[data-role='Edit']");
-            dict.Add(Delete.Split(','), "[data-role='Delete']");
+            dict.Add(obj.Add.Split(','), "[data-role='Add']");
+            dict.Add(obj.Edit.Split(','), "[data-role='Edit']");
+            dict.Add(obj.Delete.Split(','), "[data-role='Delete']");
 
             //Do not touch this!
             foreach (KeyValuePair<string[], string> col in dict)
             {
-                var condition = col.Key.ToList().Exists(f => f.Contains($"{1/*session.Role| Remove the 1 in the first index*/}"));
+                var condition = col.Key.ToList().Exists(f => f.Contains($"{1/*session?.Role*/}"));
                 if (!condition)
                 {
                     output += $"{col.Value},";
@@ -66,15 +74,15 @@ namespace ASP_MVC_Bootstrap_5_Template_v2
             if (!string.IsNullOrEmpty(output))
             {
                 string final = $"{output.Substring(0, output.Length - 1)}{{ display: none !important; }}";
-                httpContext.Response.Write($"<style>{final}</style>");
+                HttpContext.Current.Response.Write($"<style>{final}</style>");
             }
-            return true;
         }
 
-        protected override void HandleUnauthorizedRequest(AuthorizationContext filterContext)
+        void IAuthorizationFilter.OnAuthorization(AuthorizationContext filterContext)
         {
-
+            var item = (from a in filterContext.ActionDescriptor.GetCustomAttributes(true) select a).FirstOrDefault();
+            FilterCore((FilterDisplay)item);
         }
-
     }
 }
+
