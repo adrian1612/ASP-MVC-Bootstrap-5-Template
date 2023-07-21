55f4629008cb007aff44f45fa396c8f5cb58a994
ADMIN PC <ADMIN PC@DESKTOP-1QINIB3>
2023-07-22 04:44:28 +0800

    Update

--- ASP MVC Bootstrap 5 Template v2/Classes/Authorized.cs
+++ ASP MVC Bootstrap 5 Template v2/Classes/Authorized.cs
@@ -30,7 +30,11 @@ namespace ASP_MVC_Bootstrap_5_Template_v2
 
         protected override void HandleUnauthorizedRequest(AuthorizationContext filterContext)
         {
-            filterContext.Result = new RedirectToRouteResult(new System.Web.Routing.RouteValueDictionary { {"controller", "Home" }, { "action", "RestrictedAccess" } });
+            filterContext.Result = new RedirectToRouteResult(new System.Web.Routing.RouteValueDictionary
+            {
+                { "controller", "Home" },
+                { "action", "RestrictedAccess" }
+            });
         }
     }
 
