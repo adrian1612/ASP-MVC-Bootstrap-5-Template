86425f2092ec89b1e134ca56123d8eb3ab5f4c28
Furside <mj.josel1013@gmail.com>
2023-11-06 17:38:13 +0800

    installed reference for reading excel files

--- ASP MVC Bootstrap 5 Template v2/Areas/Area/Controllers/BarangayController.cs
+++ ASP MVC Bootstrap 5 Template v2/Areas/Area/Controllers/BarangayController.cs
@@ -1,8 +1,11 @@
-﻿using System;
+﻿using ASP_MVC_Bootstrap_5_Template_v2.Classes;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using ExcelDataReader.Core;
+
 
 namespace ASP_MVC_Bootstrap_5_Template_v2.Areas.Area.Controllers
 {
@@ -12,6 +15,71 @@ namespace ASP_MVC_Bootstrap_5_Template_v2.Areas.Area.Controllers
         public ActionResult Index()
         {
             return View();
+        }
+
+        public ActionResult List()
+        {
+            return AsyncResult<string>.Async(DateTime.Now.ToLongTimeString());
+        }
+
+
+        /// <summary>
+        /// Insert single item
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult Insert()
+        {
+            return AsyncResult<string>.Async(DateTime.Now.ToLongTimeString());
+        }
+
+        public ActionResult Update()
+        {
+            return AsyncResult<string>.Async(DateTime.Now.ToLongTimeString());
+        }
+
+        public ActionResult Delete()
+        {
+            return AsyncResult<string>.Async(DateTime.Now.ToLongTimeString());
+        }
+
+        /// <summary>
+        /// Insert multiple
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult MultiInsert()
+        {
+            return AsyncResult<string>.Async(DateTime.Now.ToLongTimeString());
+        }
+
+        public ActionResult MultiUpdate()
+        {
+            return AsyncResult<string>.Async(DateTime.Now.ToLongTimeString());
+        }
+
+        public ActionResult MultiDelete()
+        {
+            return AsyncResult<string>.Async(DateTime.Now.ToLongTimeString());
+        }
+
+
+        public ActionResult DropTable()
+        {
+            return AsyncResult<string>.Async(DateTime.Now.ToLongTimeString());
+        }
+
+        public ActionResult RecreateTable()
+        {
+            return AsyncResult<string>.Async(DateTime.Now.ToLongTimeString());
+        }
+
+        public ActionResult PopulateTable()
+        {
+            return AsyncResult<string>.Async(DateTime.Now.ToLongTimeString());
+        }
+
+        public ActionResult ReadExcel()
+        {
+            return AsyncResult<string>.Async(DateTime.Now.ToLongTimeString());
         }
     }
 }
