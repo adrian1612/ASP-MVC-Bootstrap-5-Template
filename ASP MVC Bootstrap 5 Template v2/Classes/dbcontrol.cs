b92c9a01da5cd9f3e9cbdbfa7f8686501dbab2ba
Admin <Admin@DESKTOP-U9H3I27>
2023-12-14 10:11:14 +0800

    Added report format

--- ASP MVC Bootstrap 5 Template v2/Classes/dbcontrol.cs
+++ ASP MVC Bootstrap 5 Template v2/Classes/dbcontrol.cs
@@ -1,4 +1,5 @@
 ﻿using ASP_MVC_Bootstrap_5_Template_v2.Models;
+using Microsoft.Reporting.WebForms;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -30,9 +31,51 @@ public class Tool
             return new SelectList(new string[] { "Male", "Female" });
         }
     }
+
+    public static byte[] ReportWrapper(string ReportPath, string Filename, ReportFormat format, Action<List<ReportDataSource>, List<ReportParameter>> _data)
+    {
+        byte[] result;
+        ReportViewer rv = new ReportViewer();
+        rv.LocalReport.ReportPath = HttpContext.Current.Server.MapPath(ReportPath);
+        List<ReportDataSource> dataSource = new List<ReportDataSource>();
+        List<ReportParameter> dataParameters = new List<ReportParameter>();
+        _data(dataSource, dataParameters);
+        dataSource.ForEach(r => rv.LocalReport.DataSources.Add(r));
+        dataParameters.ForEach(r => rv.LocalReport.SetParameters(r));
+        string Render = "", ContentType = "", Extension = "";
+        switch (format)
+        {
+            case ReportFormat.PDF:
+                Render = "PDF";
+                ContentType = "application/pdf";
+                Extension = "pdf";
+                break;
+            case ReportFormat.EXCEL:
+                Render = "EXCELOPENXML";
+                ContentType = "application/vnd.ms-excel";
+                Extension = "xlsx";
+                break;
+            default:
+                break;
+        }
+        result = rv.LocalReport.Render(Render);
+        HttpContext.Current.Response.ContentType = ContentType;
+        HttpContext.Current.Response.AddHeader("content-disposition", $"inline;filename={HttpUtility.HtmlEncode(Filename)}.{Extension}");
+        HttpContext.Current.Response.Buffer = true;
+        HttpContext.Current.Response.Clear();
+        HttpContext.Current.Response.BinaryWrite(result);
+        HttpContext.Current.Response.End();
+        return result;
+    }
 }
 
 public class UserSession
 {
     public static tbl_User User { get { return (tbl_User)HttpContext.Current.Session["User"]; } }
+}
+
+
+public enum ReportFormat
+{
+    PDF, EXCEL
 }
