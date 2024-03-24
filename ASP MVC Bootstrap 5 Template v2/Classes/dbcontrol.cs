870ea00708d3705ebc2036062fb578f36a0d1723
Lenovo <Lenovo@DESKTOP-GGN26CD>
2024-03-24 12:05:10 +0800

    Added JsonNetResult for easy conversion of DateTime in JSON

--- ASP MVC Bootstrap 5 Template v2/Classes/dbcontrol.cs
+++ ASP MVC Bootstrap 5 Template v2/Classes/dbcontrol.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Reporting.WebForms;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,6 +13,29 @@ namespace ASP_MVC_Bootstrap_5_Template_v2.Classes
         public dbcontrol() : base("ASP_MVC_Bootstrap_5_Template_v2")
         {
 
+        }
+    }
+}
+
+public class JsonNetResult : JsonResult
+{
+    public new object Data { get; set; }
+
+    public JsonNetResult()
+    {
+    }
+    public override void ExecuteResult(ControllerContext context)
+    {
+        HttpResponseBase response = context.HttpContext.Response;
+        response.ContentType = "application/json";
+        if (ContentEncoding != null)
+            response.ContentEncoding = ContentEncoding;
+        if (Data != null)
+        {
+            JsonTextWriter writer = new JsonTextWriter(response.Output) { Formatting = Formatting.Indented };
+            JsonSerializer serializer = JsonSerializer.Create(new JsonSerializerSettings());
+            serializer.Serialize(writer, Data);
+            writer.Flush();
         }
     }
 }
