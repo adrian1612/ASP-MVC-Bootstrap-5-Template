a9f4afbe2241811e4ed3ba49e7ebf028ce1e18a4
Admin <Admin@DESKTOP-U9H3I27>
2024-04-04 16:00:11 +0800

    Update

--- ASP MVC Bootstrap 5 Template v2/Controllers/HomeController.cs
+++ ASP MVC Bootstrap 5 Template v2/Controllers/HomeController.cs
@@ -74,21 +74,5 @@ namespace ASP_MVC_Bootstrap_5_Template_v2.Controllers
             Session.Clear();
             return Redirect("/");
         }
-
-        //public ActionResult Print(int ID)
-        //{
-        //    ReportViewer rv = new ReportViewer(); //EXCELOPENXML FOR EXCEL & application/vnd.ms-excel FOR contentType
-        //    rv.LocalReport.ReportPath = Server.MapPath("~/Reports/Report.rdlc");
-        //    var data = new object { };
-        //    rv.LocalReport.DataSources.Add(new ReportDataSource("Report", data));
-        //    var file = rv.LocalReport.Render("pdf");
-        //    Response.ContentType = "application/pdf";
-        //    Response.AddHeader("content-disposition", $"inline;filename=Form.pdf");
-        //    Response.Buffer = true;
-        //    Response.Clear();
-        //    Response.BinaryWrite(file);
-        //    Response.End();
-        //    return View(file);
-        //}
     }
 }
