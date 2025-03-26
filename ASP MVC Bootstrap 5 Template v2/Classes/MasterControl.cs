88205e26cc2a099129db3657eec0bb2ddb56c7d8
Admin <Admin@DESKTOP-U9H3I27>
2025-03-26 09:47:29 +0800

    Update

--- ASP MVC Bootstrap 5 Template v2/Classes/MasterControl.cs
+++ ASP MVC Bootstrap 5 Template v2/Classes/MasterControl.cs
@@ -1,15 +1,16 @@
-﻿using AAJControl;
+﻿using AJDBLib;
 using System;
 using System.Collections.Generic;
 using System.Configuration;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 
 namespace ASP_MVC_Bootstrap_5_Template_v2.Classes
 {
-    public class MasterControl : DBControl
+    public class MasterControl : AAJDBControl
     {
-        public MasterControl(string ConnectionString) : base(DatabaseType.MSSQL, ConfigurationManager.ConnectionStrings[ConnectionString].ConnectionString)
+        public MasterControl(string ConnectionString) : base(new SqlConnection(ConfigurationManager.ConnectionStrings[ConnectionString].ConnectionString))
         {
             
         }
